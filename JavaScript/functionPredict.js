42f60ab6e07c570b6e948244090aa5e2195cfed3
Kevin <kevinand1113@gmail.com>
2021-05-19 23:53:43 -0600

    5.19.21v2

--- JavaScript/functionPredict.js
+++ JavaScript/functionPredict.js
@@ -1,25 +1,25 @@
-// function greeting() {
-//     return "Hello";
-//     console.log("World");
-// }
-// var word = greeting();
-// console.log(word);
+function greeting() {
+    return "Hello";
+    console.log("World");
+}
+var word = greeting();
+console.log(word);
 
 //Output
 // Hello
 // It does not return world, because it says that line is unreachable. return is meant to be the last thing in a function.
 
-// function add(num1, num2) {
-//     console.log("Summing Numbers!");
-//     console.log("num1 is: " + num1);
-//     console.log("num2 is: " + num2);
-//     var sum = num1 + num2;
-//     return sum;
-// }
-// var result1 = add(3, 5);
-// var result2 = add(4, 7);
-// console.log(result1);
-// console.log(result2);
+function add(num1, num2) {
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    return sum;
+}
+var result1 = add(3, 5);
+var result2 = add(4, 7);
+console.log(result1);
+console.log(result2);
 
 //Output
 // Summing Numbers!
@@ -41,3 +41,5 @@ function highFive(num) {
         }
     }
 }
+
+// There is no output.  The function highFive() is never called in this code
