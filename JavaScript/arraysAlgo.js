93d0befd11d078a2a9897c8e850966c8e2000516
Kevin <kevinand1113@gmail.com>
2021-05-19 23:52:13 -0600

    5-19-21 update

--- JavaScript/arraysAlgo.js
+++ JavaScript/arraysAlgo.js
@@ -1,12 +1,9 @@
 var testArr = [6, 3, 5, 1, 2, 4];
-var total;
-for (var i = 0; i < testArr.length; i++) {
-    total = testArr[i] + testArr[i - 1];
-    console.log(testArr[i]);
-    console.log(total);
+var sum = 0;
+for (let i = 0; i < testArr.length; i++) {
+    sum = testArr[i] + sum;
+    console.log("num = ", testArr[i], "sum = ", sum);
 }
-
-//* Couldn't quite figure the one out above.  Never could quite get the right output.  Will cover in code review and update later.  The one below is showing correct output.
 
 
 var testArr = [6, 3, 5, 1, 2, 4];
